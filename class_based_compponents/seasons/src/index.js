9ddaa1008feb5622e068a9ecc0195e47b08a566e
April Copes <antdevelopment1@gmail.com>
2018-12-09 18:13:32 -0500

    converts to set up for class based component introducing state

--- class_based_compponents/seasons/src/index.js
+++ class_based_compponents/seasons/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return(
-        <div>
-            Laditude:
-            < SeasonDisplay />
-        </div>
-    )
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null
+        }
+    }
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return(<div>Laditude: </div>
+        )
+    }
 }
 
 // This example illistrates the purpose of class based components. We don't have a great way of accessing the current location of the user as that event can take much longer to
