2fc8f24c11cbffeaa70fca753ff6df61a7d11bdd
April Copes <antdevelopment1@gmail.com>
2018-12-09 17:35:57 -0500

    Adds example for why class based components are needed by using asycnhrounous events

--- class_based_compponents/seasons/src/index.js
+++ class_based_compponents/seasons/src/index.js
@@ -4,19 +4,18 @@ import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
     window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            console.log(position);
-        },
-        (err) => {
-            console.log(err);
-        }
+        (position) => console.log(position),
+        (err) => console.log(err)
     );
     return(
         <div>
-            Hello
+            Laditude:
             < SeasonDisplay />
         </div>
     )
 }
+
+// This example illistrates the purpose of class based components. We don't have a great way of accessing the current location of the user as that event can take much longer to
+// retrieve from the user than the following div being rendered. Class based components fix this issue
 
 ReactDOM.render(<App />, document.querySelector('#root'));
