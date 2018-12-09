6bb86439d0e72373b7a6934569f933b251dc6469
April Copes <antdevelopment1@gmail.com>
2018-12-08 21:10:55 -0500

    variable example for basic react component

--- app/src/index.js
+++ app/src/index.js
@@ -5,10 +5,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
+    const buttonText = 'Click Me';
     return (
-        <div>Hi. How are you</div>
+        <div>
+            <label class="label" for="name">Enter Name:</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button> 
+        </div>
     )
 }
 
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
