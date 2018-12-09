b0d1d230a2c853b988faedf557b0dba706c64f55
April Copes <antdevelopment1@gmail.com>
2018-12-08 21:12:22 -0500

    function invocation example for basic react component

--- app/src/index.js
+++ app/src/index.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+function getButton() {
+    return 'Click Something';
+}
 // Create a react component
 const App = () => {
-    const buttonText = 'Click Me';
     return (
         <div>
             <label class="label" for="name">Enter Name:</label>
             <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button> 
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButton()}</button> 
         </div>
     )
 }
