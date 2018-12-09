81306f6120d54dea0557057db8c59b8d27df3bd1
April Copes <antdevelopment1@gmail.com>
2018-12-09 13:56:25 -0500

    passes compenent as a child to another component utilising props.children

--- components/components/src/ApprovalCard.js
+++ components/components/src/ApprovalCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+
+// If we ever want to show some component or some custom content inside a component then we will take the child (CommentDetail) and wrap it with a parent component(Approval Card)
+// and then that child componentis going to show up inside the child on the props.children. It will always hsow up on the props.children 
+const ApprovalCard = (props) => {
     return (
         <div className="ui card">
-            <div>
-                Are you sure
+            <div className="content">
+                {props.children}
             </div>
             <div className="extra content">
                 <div className="ui two buttons">
--- components/components/src/index.js
+++ components/components/src/index.js
@@ -9,25 +9,30 @@ const App = () => {
     return (
         <div className="ui container comments">
              {/* WHEN USING A COMPONENT FROM OUTSIDE CURRENT FILE DONT USE CURLY BRACES USE CARROT TAGS PROPER WAY TO RENDER ONE COMPENENT INSIDE ANOTHER */}
-             <ApprovalCard />
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="Nice blog post!" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 2:00PM" 
-                content="Totally I dig it!" 
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 5:00PM" 
-                content="Man I never thought about it like that!" 
-                avatar={faker.image.avatar()}
-            /> 
+             <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00PM" 
+                    content="Totally I dig it!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    content="Man I never thought about it like that!" 
+                    avatar={faker.image.avatar()}
+                /> 
+            </ApprovalCard>
         </div>
         
     );
