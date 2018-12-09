5b0c47c422235cce9f0d5c033ac3c339f77849b1
April Copes <antdevelopment1@gmail.com>
2018-12-09 13:41:53 -0500

    imports approval card

--- components/components/src/index.js
+++ components/components/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     const image = faker.image.avatar();
     return (
         <div className="ui container comments">
              {/* WHEN USING A COMPONENT FROM OUTSIDE CURRENT FILE DONT USE CURLY BRACES USE CARROT TAGS PROPER WAY TO RENDER ONE COMPENENT INSIDE ANOTHER */}
+             <ApprovalCard />
             <CommentDetail 
                 author="Sam" 
                 timeAgo="Today at 4:45PM" 
