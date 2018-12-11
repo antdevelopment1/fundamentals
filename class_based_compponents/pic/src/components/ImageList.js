27ac4a3f41d4e56f3702c73ee64b736065616b5a
April Copes <antdevelopment1@gmail.com>
2018-12-11 17:26:45 -0500

    Adds css styles for images and pulls out logic from image list and moves it to image card

--- class_based_compponents/pic/src/components/ImageList.js
+++ class_based_compponents/pic/src/components/ImageList.js
@@ -1,19 +1,20 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard';
 // import ReactDOM from 'react-dom';
 
 const ImageList = (props) => {
     // We pluck out the values we want from image such as description id and urls so we dont have repeat ourselves inside our return for our map function
-    const images = props.images.map(({description, id, urls}) => {
+    const images = props.images.map(image => {
         // We should put the key in the element that we are returning from a list of records so if we were wrapped in a div we would put the key in the outer most div
         // Adding the alt tag and using the description from the image will get rid pf the alt warning in dev tools
-        return <img alt={description} key={id} src={urls.regular}/>
+        return <ImageCard key={image.id} image={image}/>
         // console.log(description, id, urls)
     })
     // console.log(images);
     return (
-        <div>
+        <div className="image-list">
             {images}
-            hello
         </div>
     )
 }
