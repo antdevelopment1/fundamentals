e2fd5c4107dae01ee3405c81e661cb3f5bd1b600
April Copes <antdevelopment1@gmail.com>
2018-12-11 15:07:06 -0500

    Adds extra comments for destructuring

--- class_based_compponents/pic/src/components/ImageList.js
+++ class_based_compponents/pic/src/components/ImageList.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 
 const ImageList = (props) => {
+    // We pluck out the values we want from image such as description id and urls so we dont have repeat ourselves inside our return for our map function
     const images = props.images.map(({description, id, urls}) => {
         // We should put the key in the element that we are returning from a list of records so if we were wrapped in a div we would put the key in the outer most div
         // Adding the alt tag and using the description from the image will get rid pf the alt warning in dev tools
