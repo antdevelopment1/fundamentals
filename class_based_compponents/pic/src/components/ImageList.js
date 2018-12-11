4a75386b27c37d5139521bf62c8cfe5c728e80f1
April Copes <antdevelopment1@gmail.com>
2018-12-11 15:05:47 -0500

    Plucks out wanted values from image object so we dont keep repeating ourselves

--- class_based_compponents/pic/src/components/ImageList.js
+++ class_based_compponents/pic/src/components/ImageList.js
@@ -2,10 +2,11 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 
 const ImageList = (props) => {
-    const images = props.images.map(image => {
+    const images = props.images.map(({description, id, urls}) => {
         // We should put the key in the element that we are returning from a list of records so if we were wrapped in a div we would put the key in the outer most div
         // Adding the alt tag and using the description from the image will get rid pf the alt warning in dev tools
-        return <img alt={image.description} key={image.id} src={image.urls.regular}/>
+        return <img alt={description} key={id} src={urls.regular}/>
+        // console.log(description, id, urls)
     })
     // console.log(images);
     return (
