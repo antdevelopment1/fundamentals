a991d0975b490d43161dd0bdae91d09e18ae177f
April Copes <antdevelopment1@gmail.com>
2018-12-12 12:58:35 -0500

    Pulls in the count of the list of videos into our render to confirm we are getting the list of videos to our list of videos component

--- class_based_compponents/videos/src/components/App.js
+++ class_based_compponents/videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from './../apis/youtube';
+import VideoList from './VideoList';
 
 
 // App will be a class based component because it's going to hold all the different state for our application
@@ -25,7 +26,7 @@ class App extends React.Component {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                I have {this.state.videos.length} videos.
+               <VideoList videos={this.state.videos}/>
             </div>
         )
     }
