cb64d69d0d98d3990efdfa439d6c00c1c8dfe9e3
April Copes <antdevelopment1@gmail.com>
2018-12-11 21:44:39 -0500

    pulls in axios fetch request from axios

--- class_based_compponents/videos/src/apis/youtube.js
+++ class_based_compponents/videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from axios;
+
 const KEY = AIzaSyDqIeSCDla4eA6csJdJWD1QDElr_iPGHmY;
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.om/youtube/v3',
+    params: {
+        part: snippet,
+        maxResults: 5,
+        key: KEY
+    }
+})
