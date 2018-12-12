e6200aa38a4e4daebe360c8dc418034c12a703f7
April Copes <antdevelopment1@gmail.com>
2018-12-11 21:16:42 -0500

    Adds functionality for user to type in searchbar and submit without page reloading

--- class_based_compponents/videos/src/components/SearchBar.js
+++ class_based_compponents/videos/src/components/SearchBar.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = {
+        term: ''
+    }
+
+    // This needs to be an arrow function so when we update this this. the value isnt undefined
+    inputChange = (event) => {
+
+        this.setState({
+            term: event.target.value
+        })
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Todo: Callback from parent component
+    }
 
     render() {
         return (
             <div className=" search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                     <label>Video Search</label>
-                        <input type="text"/>
+                        <input onChange={this.inputChange} type="text" value={this.state.term}/>
                     </div>
                 </form>
             </div>
