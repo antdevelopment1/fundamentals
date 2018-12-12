fe1d98786278228d83d1bd30372808d034c42a56
April Copes <antdevelopment1@gmail.com>
2018-12-12 14:30:10 -0500

    Adds in conditional statement to check if there is a video that has been clicked to be passed down to VideoDetail

--- class_based_compponents/videos/src/components/App.js
+++ class_based_compponents/videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from './../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 // App will be a class based component because it's going to hold all the different state for our application
@@ -24,13 +25,16 @@ class App extends React.Component {
     };
 
     onVideoSelect = (video) => {
-        console.log(video);
+        this.setState({
+            selectedVideo: video
+        })
     };
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.selectedVideo}/>
                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         )
