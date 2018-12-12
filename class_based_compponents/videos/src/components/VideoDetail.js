3e1d0202e8c1ac5f015efc8f13d98531db5c6942
April Copes <antdevelopment1@gmail.com>
2018-12-12 15:54:04 -0500

    Adds styles for video section and deail

--- class_based_compponents/videos/src/components/VideoDetail.js
+++ class_based_compponents/videos/src/components/VideoDetail.js
@@ -7,8 +7,13 @@ const VideoDetail = (props) => {
         )
     }
 
+    const videoSrc = `http://www.youtube.com/embed/${props.video.id.videoId}`;
+
     return (
         <div>
+            <div className="ui embed">
+                <iframe title="video player" src={videoSrc} />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">
                     {props.video.snippet.title}
--- class_based_compponents/videos/src/components/VideoItem.js
+++ class_based_compponents/videos/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = (props) => {
     console.log(props.video.snippet.thumbnails)
     return (
         <div onClick={() => {props.onVideoSelect(props.video)}} className="video-item item">
-            <img className="ui image" src={props.video.snippet.thumbnails.medium.url} />
+            <img alt={props.video.snippet.title} className="ui image" src={props.video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">
                     {props.video.snippet.title}
