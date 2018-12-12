8aa22c7573f0e7b1f8de9f2e37222d17d7d2cb67
April Copes <antdevelopment1@gmail.com>
2018-12-12 14:37:14 -0500

    Adds formatting for video description and title

--- class_based_compponents/videos/src/components/VideoDetail.js
+++ class_based_compponents/videos/src/components/VideoDetail.js
@@ -9,7 +9,14 @@ const VideoDetail = (props) => {
 
     return (
         <div>
-            {props.video.snippet.title}
+            <div className="ui segment">
+                <h4 className="ui header">
+                    {props.video.snippet.title}
+                </h4>
+                <p>
+                    {props.video.snippet.description}
+                </p>
+            </div>
         </div>
     )
 }
